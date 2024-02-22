548e5d352e4bd04e5eb298110ac1d6d6072122d3
mariamagdalena <mariamagdalenamitev@alumnat.iesdamiahuguet.net>
2024-02-22 08:16:55 +0100

    Commit 4a sessió

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 let colors = ['#ffd1dc','#ffecd1','#d1ffea','#e3d1ff','#ffbf80','#ffef96','#aaff80','#96ffe0','#ff9cf0','#d1c2ff','#b2b2b2','#99e6e6','#d98cb3','#d6ff80','#ffe080','#80ffbf']
 let symon = []
 
-function mostrarColors(){
+/*function mostrarColors(){
     for (let i = 0; i<colors.length; i++){
         if (symon.charAt(i) === colors.charAt(i)){
             console.log(colors.push(colors))
         }
     }
-}
+}*/
 /*
     Mostra la notificació al panell notification
     Mostra el color hexadecimal passat per paràmetre
@@ -42,18 +42,14 @@ function esperar(milliseconds){
 });
 }
 
-
-function colorBoto(){
-    if (){
-
+let posicio = 0
+function colorBoto(color){
+    if (color == symon[posicio]) {
+        if (symon.length() == color){
+            nouNivell()
+        }
+    }
+    else {
+        window.alert("Has perdut, el color que has pitjat no era el correcte!")
     }
 }
-/*    console.table(colors.push(colors))
-    for (let i = 0; i < .length; i++){
-        if (colors.push(colors) === symon){
-            //Que m'ensenyi un altre element de la llista
-        }
-    }*/
-
-//On he de posar el push?
-//La funció mostrar colors a q ho he d'enllaçar?
