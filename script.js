fe9638e232cf1eccffa8de43ab3d749f82d4a4f7
mariamagdalena <mariamagdalenamitev@alumnat.iesdamiahuguet.net>
2024-02-28 14:00:33 +0100

    Commit sessió 7

--- script.js
+++ script.js
@@ -43,3 +43,12 @@ function colorBoto(color){
         window.alert("Has perdut, el color que has pitjat no era el correcte!")
     }
 }
+
+var slider = document.getElementById("myRange");
+var output = document.getElementById("demo");
+output.innerHTML = slider.value; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+slider.oninput = function() {
+    output.innerHTML = this.value;
+}
