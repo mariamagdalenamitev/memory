8e44918f25c3f173bb7824475f419b9195761b63
mariamagdalena <mariamagdalenamitev@alumnat.iesdamiahuguet.net>
2024-03-05 13:07:52 +0100

    Commit darrera sessió

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let colors = ['#ffd1dc','#ffecd1','#d1ffea','#e3d1ff','#ffbf80','#ffef96','#aaff80','#96ffe0','#ff9cf0','#d1c2ff','#b2b2b2','#99e6e6','#d98cb3','#d6ff80','#ffe080','#80ffbf']
+let colors = ['#ffd1dc','#ffecd1','#bdd7f6','#e3d1ff','#ffbf80','#484014','#aaff80','#96ffe0','#ff9cf0','#846dcc','#b2b2b2','#00fdfd','#d98cb3','#d6ff80','#ffe080','#01fd82']
 let symon = []
 
 function showNotification(message) {
@@ -9,7 +9,7 @@ function showNotification(message) {
 
     setTimeout(function() {
         notification.style.display = 'none';
-    }, 2000);
+    }, document.getElementById("myRange").value);
 }
 
 //Afegir funció iniciar partida enviant el missatge. exemple:
@@ -20,7 +20,7 @@ function showNotification(message) {
      for (let i = 0; i < symon.length; i++) {
          showNotification(symon[i])
          //Crida al mètode espera
-         await esperar(2500)
+         await esperar(document.getElementById("myRange").value+500)
      }
 }
 
@@ -40,7 +40,7 @@ function colorBoto(color){
         }
     }
     else {
-        window.alert("Has perdut, el color que has pitjat no era el correcte!")
+        window.alert("Has perdut, el color que has pitjat no era el correcte! Si vols tornar a jugar reinicia la pàgina. ")
     }
 }
 
